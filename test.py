29afa6a93dbb959d3a8704e38caedc35eb688766
Dmitry Nedbaylo <dmytro.nedbaylo@gmail.com>
2016-05-23 18:00:29 +0300

    Catch a case when value cannot be encoded

--- test.py
+++ test.py
@@ -14,3 +14,23 @@ print dm_read.decode(img.size[0], img.size[1], buffer(img.tostring()))
 print dm_read.count()
 print dm_read.message(1)
 print dm_read.stats(1)
+
+
+# Invalid input value
+dm_invalid = DataMatrix(
+    module_size=2,
+    scheme=DataMatrix.DmtxSchemeC40,
+    shape=DataMatrix.DmtxSymbol44x44
+)
+INVALID_VALUE = (
+    u'JGB 6208620D02442190000000098351600   1230516230516MTA01  '
+    'RS258658803GB    DEAD REI I SOFTBANK JP             NANIWAKUSITADERA2-1-'
+    '1704  JP IG103TR  IG103TR  S                                             '
+    '                  '
+)
+try:
+    dm_invalid.encode(INVALID_VALUE)
+except ValueError, ex:
+    assert ex.message == 'Failed to encode input string.'
+else:
+    assert "Should not happen"
